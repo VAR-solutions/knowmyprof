5558053539065d350ec08cdd7aa729addeaed1d9
Ravi Varshney <ravivarshney49@gmail.com>
2018-11-19 13:34:59 +0530

    install.php

--- install.php
+++ install.php
@@ -1,9 +1,19 @@
 <?php
 
 try{
-    $connection = new PDO("mysql:host=$host", $hostname, $hostpwd, $options);
+    // $connection = new PDO("mysql:host=$host", $hostname, $hostpwd, $options);
     
-    require('config.php');
+    // require('config.php');
+    $host = "localhost";
+$hostname = "itbois";
+$hostpwd = "Password@123";
+$dbname = "it";
+$dsn = "mysql:host=$host;dbname=$dbname";
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
+$connection = new PDO($dsn,$hostname,$hostpwd,$options);
+
     $sql = file_get_contents("data/init.sql");
     $connection->exec($sql);
     echo "Database Created";
